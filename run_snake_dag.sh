d07c760b83aa647fa99f3420515f0ff5e46f26fa
Arinze-BioX <okafor.ae@gmail.com>
2024-09-06 12:08:05 -0400

    renamed pipeline file

--- run_snake_dag.sh
+++ run_snake_dag.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 source /datacommons/ydiaolab/arinze/apps/miniconda_20220118/etc/profile.d/conda.sh
-conda activate snakemake_HiCAR
+conda activate snakemake_3dstarrseq
 
-snakemake -p -s HiCARTools --forceall --rulegraph | dot -Tpdf > dag.pdf
+snakemake -p -s 3dstarrpipe --forceall --rulegraph | dot -Tpdf > dag.pdf
