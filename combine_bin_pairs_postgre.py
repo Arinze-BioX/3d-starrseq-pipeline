b01f7ed18dde1c6a463ec18bf36a9dd7750846e0
Arinze-BioX <okafor.ae@gmail.com>
2024-09-10 15:52:12 -0400

    Modified postgre code

--- combine_bin_pairs_postgre.py
+++ combine_bin_pairs_postgre.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/datacommons/ydiaolab/arinze/apps/miniconda_20220118/envs/ml_python/bin/python
 import time
 import psycopg2
 import csv
@@ -10,6 +10,7 @@ parser = argparse.ArgumentParser(description=
 (mapping to bins) into a single bin-pair. Arguments are --frag1 /path/to/bedpe/file1
 --frag2 /path/to/bedpe/file2 --output /path/to/output/file , 
 --database your_inner_db_name, --db_username your_chosen_database_username, 
+--db_password your_chosen_database_password
 The input files should have a common column which is the name of the mapped reads as the 4th column.""")
 
 # Declare arguments
@@ -20,8 +21,8 @@ parser.add_argument('--database', action="store", dest='db_name',
                     default="your_inner_db_name")
 parser.add_argument('--db_username', action="store", dest='user', 
                     default="your_chosen_database_username")
-# parser.add_argument('--db_password', action="store", dest='password', 
-#                     default="your_chosen_database_password")
+parser.add_argument('--db_password', action="store", dest='password', 
+                    default="your_chosen_database_password")
 
 # Now, parsing the command line arguments and store the 
 # values in the `args` variable
@@ -31,11 +32,14 @@ path1=args.path1
 path2=args.path2
 
 #remove existing tables
-def remove_table(table_name, db_name, user):
+def remove_table(table_name, db_name, user, password):
   try:
     with psycopg2.connect(
             database=db_name,
-            user=user
+            user=user,
+            password=password,
+            host="localhost",
+            port="5432"
         ) as conn:
 
         with conn.cursor() as cur:
@@ -45,7 +49,7 @@ def remove_table(table_name, db_name, user):
     print("Did not remove table before starting, probably because table did not exist")
 
 #load input files to already created database
-def load_data_to_postgres(tsv_file, table_name, db_name, user, column_names):
+def load_data_to_postgres(tsv_file, table_name, db_name, user, password, host, port, column_names):
     """
     Loads data from a TSV file to a PostgreSQL table using the COPY command.
 
@@ -54,12 +58,18 @@ def load_data_to_postgres(tsv_file, table_name, db_name, user, column_names):
         table_name (str): Name of the PostgreSQL table.
         db_name (str): Name of the PostgreSQL database.
         user (str): PostgreSQL username.
+        password (str): PostgreSQL password.
+        host (str): PostgreSQL host.
+        port (str): PostgreSQL port.
         column_names (str): names of columns for table.
     """
 
     with psycopg2.connect(
         database=db_name,
-        user=user
+        user=user,
+        password=password,
+        host=host,
+        port=port
     ) as conn:
         # Create empty tables
         with conn.cursor() as cur:
@@ -79,17 +89,19 @@ table_names = ["Frag_chrom", "frag_start", "frag_end", "name",
 
 #load tables to database
 remove_table('frag1_table', args.db_name, 
-                      args.user)
+                      args.user, args.password)
 load_data_to_postgres(path1, 'frag1_table', args.db_name, 
-                      args.user,table_names)
+                      args.user, args.password, 
+                      "localhost", "5432",table_names)
 
 remove_table('frag2_table', args.db_name, 
-                      args.user)
+                      args.user, args.password)
 load_data_to_postgres(path2, 'frag2_table', args.db_name, 
-                      args.user,table_names)
+                      args.user, args.password, 
+                      "localhost", "5432",table_names)
 
 #Do the cross join combination of fragments
-def cross_join_tables(db_name, user, table1, table2, needed_columns, new_table, output_file):
+def cross_join_tables(db_name, user, password, host, port, table1, table2, needed_columns, new_table, output_file):
   """
   Creates a new table by cross joining two tables with a temporary table filtering by name.
 
@@ -97,6 +109,9 @@ def cross_join_tables(db_name, user, table1, table2, needed_columns, new_table, 
     table_name (str): Name of the PostgreSQL table.
     db_name (str): Name of the PostgreSQL database.
     user (str): PostgreSQL username.
+    password (str): PostgreSQL password.
+    host (str): PostgreSQL host.
+    port (str): PostgreSQL port.
     table1: Name of the first table.
     table2: Name of the second table.
     needed_columns: list of column names to perfrom the cross join on.
@@ -105,7 +120,10 @@ def cross_join_tables(db_name, user, table1, table2, needed_columns, new_table, 
 
   with psycopg2.connect(
         database=db_name,
-        user=user
+        user=user,
+        password=password,
+        host=host,
+        port=port
     ) as conn:
     with conn.cursor() as cur:
       # Create indexes on 'name' columns
@@ -135,12 +153,13 @@ def cross_join_tables(db_name, user, table1, table2, needed_columns, new_table, 
 # Example usage:
 needed_columns = table_names
 #["name","score","bin_chrom","bin_start","bin_end","overlap"]
-#cross_join_tables(db_name, table1, table2, needed_columns, new_table)
+#cross_join_tables(db_name, user, password, host, port, table1, table2, needed_columns, new_table)
 
 remove_table('combined_bin_pair', args.db_name, 
-                      args.user)
+                      args.user, args.password)
 start = time.time()
-cross_join_tables(args.db_name, args.user, "frag1_table", 
+cross_join_tables(args.db_name, args.user, args.password, 
+                "localhost", "5432", "frag1_table", 
                 "frag2_table", needed_columns, "combined_bin_pair", args.output_file)
 end = time.time()
 print(f"It took {end - start} seconds to perform the cross join")
@@ -148,8 +167,8 @@ print(f"It took {end - start} seconds to perform the cross join")
 
 #remove created database tables
 remove_table('frag1_table', args.db_name, 
-                      args.user)
+                      args.user, args.password)
 remove_table('frag2_table', args.db_name, 
-                      args.user)
+                      args.user, args.password)
 remove_table('combined_bin_pair', args.db_name, 
-                      args.user)
+                      args.user, args.password)
