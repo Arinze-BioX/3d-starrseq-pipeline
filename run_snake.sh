47b63a774486fd3c58f4c27957067bb3e8583c81
Arinze-BioX <okafor.ae@gmail.com>
2024-09-11 16:41:20 -0400

    Fixed postgresql issue

--- run_snake.sh
+++ run_snake.sh
@@ -6,5 +6,5 @@ conda activate snakemake_3dstarrseq
 timestamp=$(date +%Y-%m-%d_%H-%M-%S)
 filename="trial_${timestamp}.log"
 
-snakemake -p --snakefile 3dstarrpipe --profile myprofile --rerun-incomplete \
+snakemake -p --snakefile 3dstarrpipe --profile myprofile --rerun-incomplete --resources cpu=1 \
 --jobs 99 --keep-going 2> "${filename}"
